f6c59bb5d57fc627016e41e372560efa0bc5b1c6
allisbri <allisbri@oregonstate.edu>
2017-10-01 04:49:51 -0400

    fixed file_sort

--- wk1/insertionsort/insertion_sort.py
+++ wk1/insertionsort/insertion_sort.py
@@ -1,7 +1,7 @@
 # Name: Brian Allison
 # Date: 9/30/2017
 # Course: CS325 Online
-# Description: Assignment 1, InsertionSort
+# Description: Assignment 1, insertion_sort
 
 
 def insertion_sort(array):
@@ -12,26 +12,26 @@ def insertion_sort(array):
             array[j + 1] = array[j]
             array[j] = key
             j = j - 1
-
     return array
 
 
 def file_sort():
     file_in = open('data.txt', 'r')
-    file_out = open()
+    file_out = open('insert.out', 'w')
     for line in file_in:
-        array_string = line.split();
+        array_string = line.split()
         array_string = array_string[1:]
-        # reference for below line to convert array of strings to ints
+        # reference for below line to convert array of strings to ints:
         # https://stackoverflow.com/questions/5306079/python-how-do-i-convert-an-array-of-strings-to-an-array-of-numbers
         array_int = [int(numeric_string) for numeric_string in array_string]
-        print(insertion_sort(array_int))
+        sorted_array = insertion_sort(array_int)
+        for s in sorted_array:
+            file_out.write(str(s))
+            file_out.write(' ')
+        file_out.write('\n')
     file_in.close()
+    file_out.close()
 
-fileOut = open('insert.out', 'w')
-'''
-array = [4, 2, 1]
 
-new_array = insertion_sort(array)
-print(new_array)
-'''
+file_sort()
+
