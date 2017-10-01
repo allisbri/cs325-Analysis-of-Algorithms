899496c747fee0d9b40ba4994a0871a15199e117
allisbri <allisbri@oregonstate.edu>
2017-10-01 04:06:41 -0400

    fixed insertion sort and added sort_file function

--- wk1/insertionsort/insertion_sort.py
+++ wk1/insertionsort/insertion_sort.py
@@ -7,15 +7,31 @@
 def insertion_sort(array):
     for i in range(1, len(array)):
         key = array[i]
-    j = i - 1
-    while key < array[j]:
-        array[i] = array[j]
-        array[j] = key
-        j = j - 1
+        j = i - 1
+        while key < array[j] and j >= 0:
+            array[j + 1] = array[j]
+            array[j] = key
+            j = j - 1
+
     return array
 
 
-array = [2, 1, 3, 9, 3]
+def file_sort():
+    file_in = open('data.txt', 'r')
+    file_out = open()
+    for line in file_in:
+        array_string = line.split();
+        array_string = array_string[1:]
+        # reference for below line to convert array of strings to ints
+        # https://stackoverflow.com/questions/5306079/python-how-do-i-convert-an-array-of-strings-to-an-array-of-numbers
+        array_int = [int(numeric_string) for numeric_string in array_string]
+        print(insertion_sort(array_int))
+    file_in.close()
+
+fileOut = open('insert.out', 'w')
+'''
+array = [4, 2, 1]
 
 new_array = insertion_sort(array)
 print(new_array)
+'''
