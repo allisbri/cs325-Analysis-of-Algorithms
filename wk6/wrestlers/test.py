fe035158fa9859bf15520bc3adcc779ef87213b4
allisbri <allisbri@oregonstate.edu>
2017-11-05 09:03:50 -0500

    finished structure of bfs

--- wk6/wrestlers/test.py
+++ wk6/wrestlers/test.py
@@ -15,7 +15,17 @@ class Wrestler:
 def wrestler_bfs(wmap, rmap):
     for i in rmap:
         if wmap[i].team == -1:
-            wmap[i].team == 0
+            Q = [i]
+            wmap[i].team = 0
+            while Q:
+                nextv = Q.pop(0)
+                for j in rmap[nextv]:
+                    if wmap[nextv].team == wmap[j].team:
+                        return
+                    elif wmap[j].team == -1:
+                        wmap[j].team = ((wmap[nextv].team - 1) * -1)
+                        Q.append(j)
+    print_maps(wmap, rmap)
 
 
 def wrestler_file_in(file_name):
@@ -35,14 +45,19 @@ def wrestler_file_in(file_name):
             rmap[rline[0]].append(rline[1])
         else:
             rmap[rline[0]] = [rline[1]]
-    print_maps(wmap, rmap)
+        if rline[1] in rmap:
+            rmap[rline[1]].append(rline[0])
+        else:
+            rmap[rline[1]] = [rline[0]]
 
     file.close()
+    print_maps(wmap, rmap)
+    wrestler_bfs(wmap, rmap)
 
 
 def print_maps(w, r):
     for i in w:
-        print(i + " " + w[i].name)
+        print(w[i].name + ' ' + str(w[i].team))
     print()
 
     print(r)
