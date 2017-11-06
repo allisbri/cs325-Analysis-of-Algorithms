f6285b2ace1b8bb391042a22f2667095c224afda
allisbri <allisbri@oregonstate.edu>
2017-11-06 00:27:00 -0500

    finished hw5

---
+++ wk6/turn_in/README.txt
@@ -0,0 +1,4 @@
+In flip, navigate to the folder with the wrestle.py file and the txt file
+Type: python3 wrestler.py 'name of txt file'
+
+example of txt file name: wrestle.txt
--- wk6/wrestlers/test.py
+++ wk6/wrestlers/test.py
@@ -1,33 +1,44 @@
 import sys
-
-# general: This class is used for activities that have a start and finish time.
-# params: identification number, a start time, and a finish time
-# conditions: Comparison operators for this class are overridden so that activities are compared by finish time
+import queue
 
 
+# general: This class is used for wrestler objects that have a name and a team number
+# params: team id number (int), name (string)
+# conditions: team id should be either -1 (no team), 0 (babyface), 1 (heel)
 class Wrestler:
-    def __init__(self, name_in, team_in, status_in):
+    def __init__(self, name_in, team_in):
         self.name = name_in
         self.team = team_in
-        self.status = status_in
 
-
+# general: Does a bfs of rmap, assigns the opposite team for rivals in wmap, if possible. If
+# opposite teams are not possible, calls a print function to let the user know. If opposite teams
+# are possible, calls a function that prints the team assignments.
+# params: wmap: a dictionary containing wrestler objects, referenced by wrestler name
+#         rmap: a dictionary containing string arrays of rivals, referenced by wrestler name
+# return: nothing returned. calls print functions to print solution details
 def wrestler_bfs(wmap, rmap):
     for i in rmap:
         if wmap[i].team == -1:
-            Q = [i]
+            q = queue.Queue()
+            q.put(i)
             wmap[i].team = 0
-            while Q:
-                nextv = Q.pop(0)
+            while not q.empty():
+                nextv = q.get()
                 for j in rmap[nextv]:
                     if wmap[nextv].team == wmap[j].team:
+                        print_no()
                         return
                     elif wmap[j].team == -1:
                         wmap[j].team = ((wmap[nextv].team - 1) * -1)
-                        Q.append(j)
+                        q.put(j)
     print_maps(wmap, rmap)
+    print_teams(wmap)
 
 
+# general: Creates a map of wrestler objects and a map of wrestler rivalry arrays consistent with the
+# rivals in the wrester object map
+# params: name of file with the wrestler information
+# return: nothing returned. calls wrestler_bfs function, passing the maps to the function
 def wrestler_file_in(file_name):
     file = open(file_name, 'r')
     w_size = int(file.readline().rstrip())
@@ -35,7 +46,7 @@ def wrestler_file_in(file_name):
     rmap = {}
     for i in range(0, w_size):
         w_name = file.readline().rstrip()
-        wrestler = Wrestler(w_name, -1, 0)
+        wrestler = Wrestler(w_name, -1)
         wmap[w_name] = wrestler
 
     r_size = int(file.readline().rstrip())
@@ -55,12 +66,32 @@ def wrestler_file_in(file_name):
     wrestler_bfs(wmap, rmap)
 
 
-def print_maps(w, r):
+# general: prints wrestler team results
+def print_teams(w):
+    babyfaces = []
+    heels = []
     for i in w:
-        print(w[i].name + ' ' + str(w[i].team))
-    print()
+        if w[i].team == 0:
+            babyfaces.append(i)
+        else:
+            heels.append(i)
 
-    print(r)
+    print('yes')
+    print('Babyfaces: ' + ', '.join(babyfaces))
+    print('Heels: ' + ', '.join(heels))
+
+
+# general: informs user that teams cannot be assigned, due to shared rivals
+def print_no():
+    print('No. At least one rivalry is shared by a pair of rivals.')
 
 
 wrestler_file_in(sys.argv[1])
+
+
+# def print_maps(w, r):
+#     for i in w:
+#         print(w[i].name + ' ' + str(w[i].team))
+#     print()
+#
+#     print(r)
